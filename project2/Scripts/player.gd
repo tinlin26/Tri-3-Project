90a8cd40b12f48f7c2664ffaee05cb56dcd66d3a
SkySloth42 <skylarh452@gmail.com>
2025-06-09 10:01:06 -0400

    .

--- project2/Scripts/player.gd
+++ project2/Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const speed = 100
 var current_dir = "none";
+var cut = false;
 
 func _physics_process(delta):
 	player_movement(delta)
@@ -9,26 +10,34 @@ func _physics_process(delta):
 func player_movement(delta):
 	
 	if Input.is_action_pressed("ui_right"):
+		cut = false;
 		current_dir = "right"
 		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
+		cut = false;
 		current_dir = "left"
 		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
+		cut = false;
 		current_dir = "down"
 		play_anim(1)
 		velocity.y = speed
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_up"):
+		cut = false;
 		current_dir = "up"
 		play_anim(1)
 		velocity.y = -speed
 		velocity.x = 0
+	elif Input.is_action_pressed("ui_select"):
+		cut = true;
+		play_anim(2)
 	else:
+		cut = false;
 		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
@@ -39,27 +48,36 @@ func play_anim(movement):
 	var dir = current_dir
 	var anim = $AnimatedSprite2D2
 	
+
 	if dir == "right":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
 			anim.play("side_idle")
+		elif movement == 2:
+			anim.play("side_cutting")
 	if dir == "left":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("side_walk")
 		elif movement == 0:
 			anim.play("side_idle")
+		elif movement == 2:
+			anim.play("side_cutting")
 	if dir == "down":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("front_walk")
 		elif movement == 0:
 			anim.play("front_idle")
+		elif movement == 2:
+			anim.play("front_cutting")
 	if dir == "up":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("back_walk")
 		elif movement == 0:
 			anim.play("back_idle")
+		elif movement == 2:
+			anim.play("back_cutting")
