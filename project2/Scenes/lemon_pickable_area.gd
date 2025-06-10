5e54de8b3ae4f573d096858f40799e64af814c85
Tina Lin <lin547661@gmail.com>
2025-06-10 17:34:31 -0400

    tried to add more pickable areas

---
+++ project2/Scenes/lemon_pickable_area.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var player_in_area = false
+var lemonCount = 0
+
+func _process(delta):
+	if player_in_area:
+		if Input.is_action_just_pressed("ui_select"):
+			lemonCount += 1
+			print("Lemon:" + lemonCount)
+			
+
+func _on_pickable_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = true
+
+
+func _on_pickable_area_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_area = false # Replace with function body.
