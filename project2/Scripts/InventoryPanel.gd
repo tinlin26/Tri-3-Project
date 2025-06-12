4bf857506fff171efbfcea062f1660584aba697e
megpun <megpun26@bergen.org>
2025-06-12 06:24:55 -0400

    Added the inventory panel with the fruits and made it so that it stays at the top of the screen
    Now just need to add the incrementing portion

---
+++ project2/Scripts/InventoryPanel.gd
@@ -0,0 +1,7 @@
+extends Sprite2D
+
+@export var margin: Vector2 = Vector2(100, 20)  # Distance from top-right (X: right, Y: down)
+
+func _ready():
+	var screen_size = get_viewport().get_visible_rect().size
+	position = Vector2(screen_size.x - margin.x, margin.y)
