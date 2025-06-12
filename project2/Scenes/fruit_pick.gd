cedba592a49dc378e87f893b0ace1abaf44af189
Tina Lin <lin547661@gmail.com>
2025-06-12 07:51:36 -0400

    NOW ITS NOT MOVING AROUND ANYMORE

--- project2/Scenes/fruit_pick.gd
+++ project2/Scenes/fruit_pick.gd
@@ -1,20 +1,37 @@
 extends Node2D
 
 var player_in_area = false
-var count = 0
+var lemonCount = 0
 
 func _process(delta):
-	if player_in_area:
-		if Input.is_action_just_pressed("ui_select"):
-			count += 1
-			print(count)
+	pass
+	#if player_in_area:
+		#if Input.is_action_just_pressed("ui_select"):
+			#count += 1
+			#print(count)
 			
 
-func _on_pickable_area_body_entered(body: Node2D) -> void:
-	if body.has_method("player"):
-		player_in_area = true
+#func _on_lemon_treeentered(body: Node2D) -> void:
+	#pass
+#
+#
+#func _on_pickable_area_body_exited(body: Node2D) -> void:
+	#if body.has_method("player"):
+		#player_in_area = false # Replace with function body.
+
+#func _on_collision_shape_2d_2_tree_entered() -> void:
+	#lemonCount += 1
+	#print("Lemons:" + lemonCount)
+#
+#func _on_collision_shape_2d_3_tree_entered() -> void:
+	#lemonCount += 1
+	#print("Lemons:" + lemonCount) # Replace with function body.
+#
+#func _on_collision_shape_2d_4_tree_entered() -> void:
+	#lemonCount += 1
+	#print("Lemons:" + lemonCount) # Replace with function body.
 
 
-func _on_pickable_area_body_exited(body: Node2D) -> void:
-	if body.has_method("player"):
-		player_in_area = false # Replace with function body.
+func _on_lemon_pickable_area_tree_entered() -> void:
+	lemonCount += 1
+	print("Lemons: " + lemonCount) # Replace with function body.
