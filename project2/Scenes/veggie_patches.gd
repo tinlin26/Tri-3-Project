bb7a433fbdcc9ecbb52dae2ced346d9c533370da
SkySloth42 <skylarh452@gmail.com>
2025-06-14 18:43:19 -0400

    .

--- project2/Scenes/veggie_patches.gd
+++ project2/Scenes/veggie_patches.gd
@@ -1,36 +1,29 @@
 extends Node2D
 
 var player_in_area = false
-var blueCount = 0
+var veggieCount = 0
 var inventory_canvas: Node = null
 
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	# Update with your actual CanvasLayer path (copy from editor if unsure)
 	inventory_canvas = get_node("../CanvasLayer")
-
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if player_in_area and Input.is_action_just_pressed("e"):
-		blueCount += 1
-		print("Blueberries picked:" + str(blueCount))
+		veggieCount += 1
+		print("Veggies picked:" + str(veggieCount))
 		
 		if inventory_canvas:
-			inventory_canvas.update_blueberry_count(blueCount)
+			inventory_canvas.update_veggie_count(veggieCount)
 
 func _on_pickable_area_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_in_area = true
-
+		
 
 func _on_pickable_area_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_in_area = false
-
-
-#func _on_pickable_area_2_body_entered(body: Node2D) -> void:
-	#if body.has_method("player"):
-		#player_in_area = true # Replace with function body.
-#
-#
-#func _on_pickable_area_2_body_exited(body: Node2D) -> void:
-	#if body.has_method("player"):
-		#player_in_area = false # Replace with function body.
